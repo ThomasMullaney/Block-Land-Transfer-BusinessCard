d88f78f45b43ea1fd985c9abe2e196f09705a14c
Thomas Mullaney <tloring.mullaney@gmail.com>
2021-01-11 11:57:43 -0800

    working on setting up pages and creating skeleton of components

--- website-1/src/App.js
+++ website-1/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
+import Fade from "react-reveal/Fade";
+import {Parallax} from "react-parallax";
+import Conainer from "react-bootstrap/Container";
+import MyCarousel from "./components/carousel/carousel.component";
+import Title from "./components/title/title.component";
+import Navbar from "./components/navbar/navbar.component";
+import Footer from "./components/footer/footer.component";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ postion: "relative"}}>
+      <MyCarousel />
+      <Title />
+      <Navbar />
+      <div>
+        <Container className="container-box rounded">
+          <Fade duration={500}>
+            <About />
+          </Fade>
+        </Container>
+      </div>
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <Services />
+        </Slide>
+      </Container>
+      <div>
+      <Container className="container-box rounded">
+        <Fade duration={500}>
+          <hr />
+        </Fade>
+      </Container>
+      </div>
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          {/* <Timeline */}
+        </Slide>
+      </Container>
+      <Container className="container-box rounded">
+        <Fade duration={500}>
+          <hr />
+          <Contact />
+        </Fade>
+      </Container>
+
+      <hr />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
